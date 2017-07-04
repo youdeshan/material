c6953c70dbae341dc76cbff2a14a11c029e1b2a7
StevenYou <StevenYou@gmail.com>
2017-07-04 22:13:35 +0800

    element buffer object

--- opengl/sample/src/app/main.cpp
+++ opengl/sample/src/app/main.cpp
@@ -85,14 +85,21 @@ int main(int argc, char* argv[]) {
 //  }
   GLuint program = LoadShaders(shaders);
   GLfloat vertices[] = {
-      -0.5f, -0.5f, 0.0f,
-      0.5f, -0.5f, 0.0f,
-      0.0f, 0.5f, 0.0f
+      0.5f, 0.5f, 0.0f, // top right
+      0.5f, -0.5f, 0.0f,// bottom right
+      -0.5f, -0.5f, 0.0f, // bottom left
+      -0.5f, 0.5f, 0.0f // top left
   };
 
-  GLuint VAO, VBO;
+  GLuint indices[] = {
+      0, 1, 3,
+      1, 2, 3
+  };
+
+  GLuint VAO, VBO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -100,6 +107,9 @@ int main(int argc, char* argv[]) {
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
   glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
