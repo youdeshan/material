2f7108dd6af433c5fd52eec9cb55f2ab86fd8ed8
stevenyou <youdeshan@gmail.com>
2018-02-02 18:20:37 +0800

    add notepad implement

--- qt/samples/src/mainwindow.cpp
+++ qt/samples/src/mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
 #include <QMessageBox>
+#include <qfile.h>
+#include <qtextstream.h>
+#include <QFileInfo>
+#include <QFileDialog>
+#include <QCloseEvent>
 
 #include "user_info_window.h"
 #include "user_info_window_ext.h"
@@ -14,8 +20,8 @@ MainWindow::MainWindow(QWidget *parent) :
 
     ui->setupUi(this);
 
-    ui->label->setText(u8"Hello, welcome to QT world! 欢迎游德山！");
-    ui->menu_file->addSeparator();
+    //ui->label->setText(u8"Hello, welcome to QT world! 欢迎游德山！");
+    //ui->menu_file->addSeparator();
 
     /* Add menu item(QAction) for save */
     //QAction* save_action = new QAction(tr("&Save(&S)"), this);
@@ -28,6 +34,9 @@ MainWindow::MainWindow(QWidget *parent) :
 
     //ui->mainToolBar->addSeparator();
     //ui->mainToolBar->addAction(save_action);
+    is_untitled_ = true;
+    cur_file_name_ = tr("Unname.txt");
+    setWindowTitle(cur_file_name_);
 }
 
 MainWindow::~MainWindow()
@@ -36,20 +45,68 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::OnNewFile() {
-    QDialog* dlg = new QDialog(this);
-    dlg->setMinimumSize(QSize(300, 200));
-    dlg->setWindowTitle(tr("New file dialog"));
-    dlg->setAttribute(Qt::WA_DeleteOnClose); // destroy the dialog when closing the dialog
-    dlg->show();
+    //QDialog* dlg = new QDialog(this);
+    //dlg->setMinimumSize(QSize(300, 200));
+    //dlg->setWindowTitle(tr("New file dialog"));
+    //dlg->setAttribute(Qt::WA_DeleteOnClose); // destroy the dialog when closing the dialog
+    //dlg->show();
+    if (MaybeSave()) {
+        is_untitled_ = true;
+        cur_file_name_ = tr("Unname.txt");
+        setWindowTitle(cur_file_name_);
+
+        ui->textEdit->clear();
+        ui->textEdit->setVisible(true);
+    }
+}
+
+void MainWindow::OnOpenFile() {
+    if (MaybeSave()) {
+        QString file_name = QFileDialog::getOpenFileName(this, tr("Open file"));
+        if (file_name.isEmpty()) return;
+
+        LoadFile(file_name);
+        ui->textEdit->setVisible(true);
+    }
 }
 
 void MainWindow::OnSaveFile() {
-    //QMessageBox::information(this, tr("Info"), tr("Open"));
-    QDialog* dlg = new QDialog(this);
-    dlg->setMinimumSize(QSize(300, 200));
-    dlg->setWindowTitle(tr("Save file dialog"));
-    dlg->setAttribute(Qt::WA_DeleteOnClose); // destroy the dialog when closing the dialog
-    dlg->show();
+    //QDialog* dlg = new QDialog(this);
+    //dlg->setMinimumSize(QSize(300, 200));
+    //dlg->setWindowTitle(tr("Save file dialog"));
+    //dlg->setAttribute(Qt::WA_DeleteOnClose); // destroy the dialog when closing the dialog
+    //dlg->show();
+
+    Save();
+}
+
+void MainWindow::OnSaveAsFile() {
+    SaveAs();
+}
+
+void MainWindow::OnCloseFile() {
+    if (MaybeSave()) ui->textEdit->setVisible(false);
+}
+
+void MainWindow::OnExit() {
+    if (MaybeSave()) ui->textEdit->setVisible(false);
+    qApp->quit();
+}
+
+void MainWindow::OnUndo() {
+    ui->textEdit->undo();
+}
+
+void MainWindow::OnCut() {
+    ui->textEdit->cut();
+}
+
+void MainWindow::OnCopy() {
+    ui->textEdit->copy();
+}
+
+void MainWindow::OnPaste() {
+    ui->textEdit->paste();
 }
 
 void MainWindow::test_click() {
@@ -64,4 +121,73 @@ void MainWindow::on_user_info_click() {
 void MainWindow::on_user_info_ext_click() {
     UserInfoWindowExt* user_info_win = new UserInfoWindowExt(this);
     user_info_win->show();
+}
+
+bool MainWindow::MaybeSave() {
+    if (ui->textEdit->document()->isModified()) {
+        int ret = QMessageBox::warning(this, tr("Multiple text editor"),
+            tr("The document has been modified.\n""Do you want to save your changes?"),
+            QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel,
+            QMessageBox::Save);
+
+        switch (ret) {
+        case QMessageBox::Save:
+            return Save();
+        case QMessageBox::Cancel:
+            return false;
+        default:
+            break;
+        }
+    }
+    return true;
+}
+
+bool MainWindow::Save() {
+    return is_untitled_ ? SaveAs() : SaveFile(cur_file_name_);
+}
+
+bool MainWindow::SaveAs() {
+    QString file_name = QFileDialog::getSaveFileName(this, tr("Save as"), cur_file_name_);
+    if (file_name.isEmpty()) return false;
+    return SaveFile(file_name);
+}
+
+bool MainWindow::SaveFile(const QString& file_name) {
+    QFile file(file_name);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("Multiple document editor"), tr("Cannot write file %1: \n %2.").arg(file_name).arg(file.errorString()));
+        return false;
+    }
+
+    QTextStream out(&file);
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    out << ui->textEdit->toPlainText();
+    QApplication::restoreOverrideCursor();
+    
+    is_untitled_ = false;
+    cur_file_name_ = QFileInfo(file_name).canonicalFilePath();
+    setWindowTitle(cur_file_name_);
+    return true;
+}
+
+bool MainWindow::LoadFile(const QString& file_name) {
+    QFile file(file_name);
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("Multiple document editor"), tr("Cannot read file %1: \n %2.").arg(file_name).arg(file.errorString()));
+        return false;
+    }
+
+    QTextStream in(&file);
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    ui->textEdit->setPlainText(in.readAll());
+    QApplication::restoreOverrideCursor();
+
+    is_untitled_ = false;
+    cur_file_name_ = QFileInfo(file_name).canonicalFilePath();
+    setWindowTitle(cur_file_name_);
+    return true;
+}
+
+void MainWindow::closeEvent(QCloseEvent *event) {
+    MaybeSave() ? event->accept() : event->ignore();
 }
--- qt/samples/src/mainwindow.h
+++ qt/samples/src/mainwindow.h
@@ -7,6 +7,7 @@ namespace Ui {
 class MainWindow;
 }
 
+class QCloseEvent;
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
@@ -20,10 +21,30 @@ private slots:
     void on_user_info_click();
     void on_user_info_ext_click();
     void OnNewFile();
+    void OnOpenFile();
     void OnSaveFile();
+    void OnSaveAsFile();
+    void OnCloseFile();
+    void OnExit();
+    void OnUndo();
+    void OnCut();
+    void OnCopy();
+    void OnPaste();
+
+private:
+    bool MaybeSave();
+    bool Save();
+    bool SaveAs();
+    bool SaveFile(const QString& file_name);
+    bool LoadFile(const QString& file_name);
+
+    void closeEvent(QCloseEvent *event);
 
 private:
     Ui::MainWindow *ui;
+
+    bool is_untitled_;
+    QString cur_file_name_;
 };
 
 #endif // MAINWINDOW_H
